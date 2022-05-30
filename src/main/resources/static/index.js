dadd91123c6febef1e4fa580cd5486b4cfb90270
Brage Eilertsen <s364742@oslomet.no>
2022-05-30 21:32:18 +0200

    Initial commit

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,31 @@
+function scrollned() {
+    window.scrollTo(300, 900);
+}
+
+function scrollned2() {
+    window.scrollTo(300, 1700);
+}
+
+function scrollned3() {
+    window.scrollTo(300, 2500);
+}
+
+function scrollned4() {
+    window.scrollTo(300, 3300);
+}
+
+function scrollned5() {
+    window.scrollTo(300, 4100);
+}
+
+function scrollopp5() {
+    window.scrollTo(300, 2500);
+}
+
+function scrollned6() {
+    window.scrollTo(300, 4900);
+}
+
+function scrollopp6() {
+    window.scrollTo(300, 3300);
+}
